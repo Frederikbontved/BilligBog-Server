85a5a43580142b2ca949c988e13d821f2b4c9362
Frederik Larsen <202005603@post.au.dk>
2023-02-19 15:43:40 +0100

    Changed port to 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 const connectDB = async () => {
